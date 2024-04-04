f9b89034d8c8ce04d41236e7f59cfaca10b8ba31
Nandhini_T <rtnandhutee@gmail.com>
2024-04-04 22:24:02 +0530

    first update

--- app/page.js
+++ app/page.js
@@ -1,31 +1,34 @@
-'use client'
-import React,{useRef, useState} from 'react'
-import InputField from './InputField'
+import React, { useRef, useState } from 'react';
+import InputField from './InputField';
 
 function Home() {
+  const inputRef =useRef();
+  const [values, setValues] = useState('');
 
-  const newRef=useRef();
-  const [values,setValues] = useState("")
+  const settingValue =() => {
+    const inputValue =inputRef.current.value;
+    setValues(inputValue);
+  };
 
-  const settingValue = () => {
-
-  }
-
-  const focusInput = () => {
-
-  }
-
+  const focusInput =() => {
+    inputRef.current.focus();
+  };
 
   return (
     <div>
-    <InputField  ref={newRef}  type="text"/>
-    <br/>
-    <button id="settingValueButton" onClick={settingValue}>Set Value</button>
-    <button id="focusInputButton" onClick={focusInput}>Focus the input</button><br/><br/>
-     <textarea id="textarea" value={values}></textarea>
-
+      <InputField ref={inputRef} type="text" />
+      <br />
+      <button id="settingValueButton" onClick={settingValue}>
+        Set Value
+      </button>
+      <button id="focusInputButton" onClick={focusInput}>
+        Focus the input
+      </button>
+      <br />
+      <br />
+      <textarea id="textarea" value={values}></textarea>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
